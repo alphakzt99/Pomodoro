7d379563436ea1f723e3a0a20f2c4c9fb9dee514
Kaung Zaw Thant <kaung.thant@aiesec.net>
2022-10-29 16:15:50 -0500

    Timer finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,11 +12,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Pomodoro',
       theme: ThemeData(
-        primaryColor: Colors.black,
-        primaryColorLight: Color(0xFF7AE582),
-      ),
+          primaryColor: Colors.black,
+          primaryColorLight: Color(0xFF7AE582),
+          primaryColorDark: Colors.white),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
@@ -31,11 +33,34 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
-  countText() {
-    return Duration();
+  String get countText {
+    Duration count = controller.duration! * controller.value;
+    return controller.isDismissed
+        ? '${(controller.duration!.inHours % 24).toString().padLeft(2, '0')}:${(controller.duration!.inMinutes % 60).toString().padLeft(2, '0')}:${(controller.duration!.inSeconds % 60).toString().padLeft(2, '0')}'
+        : '${(count.inHours % 24).toString().padLeft(2, '0')}:${(count.inMinutes % 60).toString().padLeft(2, '0')}:${(count.inSeconds % 60).toString().padLeft(2, '0')}';
   }
 
-  late AnimationController controller = AnimationController(vsync: this);
+  double progress = 1.0;
+  late AnimationController controller;
+  @override
+  void initState() {
+    controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 60));
+    controller.addListener(() {
+      if (controller.isAnimating) {
+        setState(() {
+          progress = controller.value;
+        });
+      } else {
+        setState(() {
+          progress = 1.0;
+        });
+      }
+    });
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   void dispose() {
     controller.dispose();
@@ -46,6 +71,8 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
+
+    bool isCounting = false;
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -74,6 +101,110 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
             width: size.width,
             height: size.height,
             decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+            child: Column(children: [
+              Expanded(
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    SizedBox(
+                      height: 300,
+                      width: 300,
+                      child: CircularProgressIndicator(
+                        backgroundColor: Theme.of(context).primaryColorDark,
+                        color: Theme.of(context).primaryColorLight,
+                        value: progress,
+                        strokeWidth: 6,
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        if (controller.isDismissed) {
+                          showModalBottomSheet(
+                              context: context,
+                              builder: (context) => CupertinoTimerPicker(
+                                    initialTimerDuration: controller.duration!,
+                                    onTimerDurationChanged: (value) {
+                                      setState(() {
+                                        controller.duration = value;
+                                      });
+                                    },
+                                  ));
+                        }
+                      },
+                      child: AnimatedBuilder(
+                        animation: controller,
+                        builder: (context, child) => Text(
+                          countText,
+                          style: TextStyle(
+                              fontSize: 60,
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).primaryColorLight),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              MaterialButton(
+                  elevation: 5,
+                  minWidth: 100,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  color: Theme.of(context).primaryColorLight,
+                  onPressed: () {
+                    if (controller.isAnimating) {
+                      controller.stop();
+                      setState(() {
+                        isCounting = false;
+                      });
+                    } else {
+                      controller.reverse(
+                          from: controller.value == 0 ? 1.0 : controller.value);
+                      setState(() {
+                        isCounting = true;
+                      });
+                    }
+                  },
+                  child: isCounting == false
+                      ? Text(
+                          "Start the timer",
+                          style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        )
+                      : Text(
+                          "Stop the timer",
+                          style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        )),
+              MaterialButton(
+                elevation: 5,
+                minWidth: 100,
+                shape: RoundedRectangleBorder(
+                    side:
+                        BorderSide(color: Theme.of(context).primaryColorLight),
+                    borderRadius: BorderRadius.circular(20)),
+                color: Theme.of(context).primaryColor,
+                onPressed: () {
+                  if (controller.isAnimating) {
+                    controller.reset();
+                    setState(() {
+                      isCounting = false;
+                    });
+                  }
+                },
+                child: Text(
+                  "Reset the timer",
+                  style: TextStyle(
+                      color: Theme.of(context).primaryColorLight,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ]),
           )),
     );
   }
