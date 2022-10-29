27897d2b21c935f4c52968351a8757d0d2ceff30
Kaung Zaw Thant <kaung.thant@aiesec.net>
2022-10-28 22:54:45 -0500

    startfirstpage

--- lib/main.dart
+++ lib/main.dart
@@ -30,36 +30,51 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  countText() {
+    return Duration();
+  }
+
+  late AnimationController controller = AnimationController(vsync: this);
+  @override
+  void dispose() {
+    controller.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            onPressed: () {
-              Drawer(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(20),
-                        bottomRight: Radius.circular(20))),
-                backgroundColor: Theme.of(context).primaryColor,
-                child: Column(children: [
-                  
-                ]),
-              );
-            },
-            icon: Icon(
-              Icons.account_circle_outlined,
-              color: Theme.of(context).primaryColorLight,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Theme.of(context).primaryColor,
+            leading: IconButton(
+              onPressed: () {
+                Drawer(
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(20),
+                          bottomRight: Radius.circular(20))),
+                  backgroundColor: Theme.of(context).primaryColor,
+                  child: Column(children: []),
+                );
+              },
+              icon: Icon(
+                Icons.account_circle_outlined,
+                color: Theme.of(context).primaryColorLight,
+              ),
             ),
           ),
-        ),
-        body: Container(
-          width: size.width,
-          height: size.height,
-          
-        ));
+          body: Container(
+            width: size.width,
+            height: size.height,
+            decoration: BoxDecoration(color: Theme.of(context).primaryColor),
+          )),
+    );
   }
 }
