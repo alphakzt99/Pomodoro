4a25d41a06ae35fd2c4584c4d01ec790f5d51da2
Kaung Zaw Thant <66521022+kztJames01@users.noreply.github.com>
2025-03-06 08:12:01 -0600

    Timer updated id deleted

--- lib/timer.dart
+++ lib/timer.dart
@@ -1,26 +1,23 @@
 import 'package:intl/intl.dart';
 
 class Timer {
-  int id;
   String title;
   String timer;
   String datetime;
   
-  Timer({this.id = 1, this.title = '', this.timer = '', this.datetime = ''}){
+  Timer({this.title = '', this.timer = '', this.datetime = ''}){
     if(datetime.isEmpty){
       datetime = DateFormat.yMMMEd().format(DateTime.now());
     }
   }
 
-  Timer.withID({required this.id, this.title = '', this.timer = '', this.datetime = ''});
 
   Timer.fromMap(Map<String, dynamic> map)
-      : id = map['id'] ?? 1,
+      : 
         title = map['title'] ?? '',
         timer = map['timer'] ?? '',
         datetime = map['dateTime'] ?? DateFormat.yMMMEd().format(DateTime.now());
   Map<String, dynamic> toMap() => {
-        'id': id,
         'title': title,
         'timer': timer,
         'dateTime': datetime,
